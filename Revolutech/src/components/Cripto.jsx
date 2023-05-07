9ca3f4dcb0daf82d2045cc21c0386050e72de641
wendel <wh68639@gmail.com>
2023-05-07 17:32:43 -0300

    em andamento

--- Revolutech/src/components/Cripto.jsx
+++ Revolutech/src/components/Cripto.jsx
@@ -5,16 +5,20 @@ import axios from 'axios'
 const Cripto = () => {
     
   return (
-    <div className='w-4/5 mx-auto'>
-        <div>
-            <h2>Moeda</h2>
-            <p>Bitcoin</p>
-            <p>Ether</p>
-            <p>XRP</p>
-            <p>Cardano</p>
-            <p>Dogecoin</p>
-            <p>Polygon</p>
-            <p>Solana</p>
+    <div className='w-4/5 mx-auto mt-[5%]'>
+        <div className='w-[25%]'>
+            <h2 className='border-b border-gray pb-6 pt-2 text-sm text-gray'>Moeda</h2>
+            <p className='border-b border-gray pb-6 pt-2'>Bitcoin</p>
+            <p className='border-b border-gray pb-6 pt-2'>Ether</p>
+            <p className='border-b border-gray pb-6 pt-2'>XRP</p>
+            <p className='border-b border-gray pb-6 pt-2'>Cardano</p>
+            <p className='border-b border-gray pb-6 pt-2'>Dogecoin</p>
+            <p className='border-b border-gray pb-6 pt-2'>Polygon</p>
+            <p className='border-b border-gray pb-6 pt-2'>Solana</p>
+        </div>
+
+        <div className='w-[19%] ml-[1%]'>
+
         </div>
     </div>
   )
