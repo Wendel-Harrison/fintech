987d7dd348eb94ce817c0cfc198a1f2bd0b88e2d
wendel <wh68639@gmail.com>
2023-05-07 22:35:06 -0300

    ta safe

--- Revolutech/src/components/Footer.jsx
+++ Revolutech/src/components/Footer.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
+import { BsInstagram, BsTwitter } from 'react-icons/bs'
+import { RiLinkedinFill } from 'react-icons/ri'
+import { FaFacebookF, FaTelegramPlane } from 'react-icons/fa'
+import { AiFillInstagram } from 'react-icons/ai'
+
+import visa from '../assets/visa.png'
+import master from '../assets/master.png'
+import master2 from '../assets/master2.png'
+import apay from '../assets/apay.png'
+import gpay from '../assets/gpay.png'
+import paypal from '../assets/paypal.png'
 
 const Footer = () => {
   return (
     <div className='bg-[#5C0000]'>
-        <h1 className='text-center text-2xl'>RevoluTech</h1>
+        <h1 className='text-center text-2xl text-white font-semibold pt-20 animate-bounce'>RevoluTech</h1>
+
+        <div className='flex gap-3 mt-3 justify-center '>
+            <AiFillInstagram className='py-1 px-1 text-2xl text-[#5c0000] bg-white rounded-full hover:scale-150 hover:bg-[#5c0000] hover:text-white transition-all duration-200'/>
+            <BsTwitter className='py-1 px-1 text-2xl text-[#5c0000] bg-white rounded-full hover:scale-150 hover:bg-[#5c0000] hover:text-white transition-all duration-200'/>
+            <RiLinkedinFill className='py-1 px-1 text-2xl text-[#5c0000] bg-white rounded-full hover:scale-150 hover:bg-[#5c0000] hover:text-white transition-all duration-200'/>
+            <FaFacebookF className='py-1 px-1 text-2xl text-[#5c0000] bg-white rounded-full hover:scale-150 hover:bg-[#5c0000] hover:text-white transition-all duration-200'/>
+            <FaTelegramPlane className='py-1 px-1 text-2xl text-[#5c0000] bg-white rounded-full hover:scale-150 hover:bg-[#5c0000] hover:text-white transition-all duration-200'/>
+        </div>
+
+        <div className='flex gap-5 mt-8 justify-center'>
+            <img src={visa} alt=""  className='hover:translate-y-2 transition-all duration-100'/>
+            <img src={master} alt=""  className='hover:translate-y-2 transition-all duration-100'/>
+            <img src={master2} alt=""  className='hover:translate-y-2 transition-all duration-100'/>
+            <img src={apay} alt="" className='hover:translate-y-2 transition-all duration-100' />
+            <img src={gpay} alt=""  className='hover:translate-y-2 transition-all duration-100'/>
+            <img src={paypal} alt=""  className='hover:translate-y-2 transition-all duration-100'/>
+        </div>
+
+        <p className='text-center text-white font-thin mt-10 pb-2 text-sm'>All rights reserved © 2023 Bitstamp. NMLS #1905429</p>
     </div>
   )
 }
--- Revolutech/src/components/Header.jsx
+++ Revolutech/src/components/Header.jsx
@@ -4,16 +4,16 @@ const Header = () => {
   return (
     <div className='font-inter flex justify-between items-center px-14 py-2 bg-[#f2f2f2]'>
         <div>
-            <h1 className=' text-red font-semibold text-xl'>RevoluTech</h1>
+            <h1 className=' text-red font-semibold text-xl hover:text-red1 transition-all duration-200 hover:scale-110 active:translate-y-2'>RevoluTech</h1>
         </div>
         <div className='flex gap-5'>
-            <h2 className='text-gray text-sm'>Aplicativo Mobile</h2>
-            <h2 className='text-gray text-sm'>Mercado</h2>
-            <h2 className='text-gray text-sm'>Criptomoedas</h2>
+            <h2 className='text-gray text-sm hover:text-black transition-all duration-150 hover:scale-110 hover:font-medium'>Aplicativo Mobile</h2>
+            <h2 className='text-gray text-sm hover:text-black transition-all duration-150 hover:scale-110 hover:font-medium'>Mercado</h2>
+            <h2 className='text-gray text-sm hover:text-black transition-all duration-150 hover:scale-110 hover:font-medium'>Criptomoedas</h2>
         </div>
         <div className='flex items-center gap-5 '>
-            <h2 className='text-gray underline-offset-8 font-medium'>Login</h2>
-            <h2 className='bg-red py-1 px-4 rounded-sm text-white font-medium'>Começar</h2>
+            <h2 className='text-gray underline-offset-4 font-medium hover:underline hover:text-black transition-all duration-200'>Login</h2>
+            <h2 className='hover:text-red1 hover:bg-white transition-all duration-100 bg-red py-1 px-4 rounded-sm text-white font-medium'>Começar</h2>
         </div>
     </div>
   )
